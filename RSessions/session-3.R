c4e9bfad56275db3f067f384748a079ad9fabbea
Tanvir <tanvirhossain.sk@gmail.com>
2025-07-07 22:54:33 +0600

    added estimation updated slides

---
+++ RSessions/session-3.R
@@ -0,0 +1,55 @@
+
+# You can practice your R skill with the following commands
+
+
+# R has different kinds of objects
+
+# Type - Character / String
+test1 <- "ECO104"
+test1
+class(test1)
+
+
+
+# Type - Numeric Objects
+
+test2 <- 2
+test2
+class(test2)
+
+
+test3 <- c(1, 2, 3)
+test3
+class(test3)
+
+
+# Type - Lists
+test4 <- list(1, "all", c(1, 2))
+class(test4)
+
+
+
+# Type - Dataframe 
+test5 <- data.frame (
+  grade = c("A", "B", "C"),
+  marks = c(90, 80, 70),
+  semester = c(5, 4, 4)
+)
+
+# Print the data frame
+test5
+
+class(test5)
+
+
+# Type - function
+
+myfunction <- function(x){
+  
+}
+
+test6(2)
+
+class(test6)
+
+
